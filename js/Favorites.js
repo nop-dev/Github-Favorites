86c22058324a7c6a8f1516c0fd92c4ba0c808c99
Yuri Sousa <79167904+Nop-Dev@users.noreply.github.com>
2024-04-03 16:40:20 -0300

    feat: :sparkles: toda a lógica da apresentação dos dados, dados fictícios adicionados

--- js/Favorites.js
+++ js/Favorites.js
@@ -2,7 +2,25 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root);
+        this.load();
     };
+    
+    load() {
+        this.entries = [
+            {
+                login: "maykbrito",
+                name: "Mayk Brito",
+                public_repos: "98",
+                followers: "12k",      
+            },
+            {
+                login: "Nop-Dev",
+                name: "Yuri Sousa",
+                public_repos: "33",
+                followers: "12"    ,        
+            }
+        ]
+    }
 };
 
 // classe pra construir o HTML
@@ -11,18 +29,53 @@ export class FavoritesView extends Favorites {
         super(root);
 
         console.log(this.root);
+        this.tbody = this.root.querySelector("table tbody");
 
         this.update();
-    }
+    };
 
     update() {
         this.removeAllTr();
+
+        this.entries.forEach( user => {
+            const row = this.createRow();
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`;
+            row.querySelector('.user img').alt = `Imagem de ${user.login}/${user.name}`;
+            row.querySelector('.user a').href = `https://github.com/${user.login}`;
+            row.querySelector('.user p').textContent = `${user.name}`;
+            row.querySelector('.user span').textContent = `${user.login}`;
+            row.querySelector('.repositories').textContent = `${user.public_repos}`;
+            row.querySelector('.followers').textContent = `${user.followers}`;
+
+            this.tbody.append(row);
+        })
     };
 
+    createRow() {
+        const tr = document.createElement('tr');
+
+        const content = `
+           <td class="user">
+               <img src="" alt="">
+               <a href="" target="_blank">
+                   <p></p>
+                    <span></span>
+               </a>
+            </td>
+            <td class="repositories"></td>
+            <td class="followers"></td>
+            <td>
+                <button class="remove">&times;</button>
+            </td>
+    `
+        tr.innerHTML = content;
+
+        return tr
+    }
+
     removeAllTr() {
-        const tbody = this.root.querySelector("table tbody");
-      
-        tbody.querySelectorAll('tr')
+        this.tbody.querySelectorAll('tr')
             .forEach((tr) => {
             tr.remove();
         });
